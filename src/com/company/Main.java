2a08c7a061656075c2f0c57dbd343a4f1b28fbff
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2021-11-29 11:56:45 +0100

    main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,23 @@
 package com.company;
 
+import java.lang.reflect.Array;
+
 public class Main {
-
     public static void main(String[] args) {
-	int prix_final;
+        float prix = 10;
+        float remise = 50;
+        float remise2 = 30;
+        prix_remise(prix, remise, remise2);
 
+    }
 
-
-
+    public static void prix_remise(float prix, float remise, float remise2) {
+        float remise_1 = (int) Math.floor(prix * (1 - remise / 100.f));
+        System.out.println("remise 1 = " + remise_1);
+        float remise_2 = (int) Math.floor(remise_1 * (1 - remise2 / 100.f));
+        System.out.println("remise 2 = " + remise_2);
     }
 }
 
 
-/*
-algo qui calcul prix final d'un produit a 10euro sur lequel on applique successivement une remise de 50% puis de 30%
-le faire d'abord en dur
-creer une fonction pour le faire
- */
+
