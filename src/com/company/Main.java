d292e19b591e1c760aa2aa1c3c88322e780f054a
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2021-11-29 09:45:03 +0100

    main

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int prix_final;
+
+
+
+
+    }
+}
+
+
+/*
+algo qui calcul prix final d'un produit a 10euro sur lequel on applique successivement une remise de 50% puis de 30%
+le faire d'abord en dur
+creer une fonction pour le faire
+ */
